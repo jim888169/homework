ec27b1ec97041e5271adaa3c0696e8ba931470d0
jim888169 <gyyg25468402@gmail.com>
2024-12-27 03:25:38 +0800

    homework

---
+++ MVC_HW/MVC_HW/Controllers/TaskController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC_HW.Data;
+using MVC_HW.Models;
+
+namespace MVC_HW.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly TaskContext _context;
+
+        public TaskController(TaskContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Task.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TaskId,Title,Description,Budget,DueDate")] Models.Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Task.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TaskId,Title,Description,Budget,DueDate")] Models.Task task)
+        {
+            if (id != task.TaskId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.TaskId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Task
+                .FirstOrDefaultAsync(m => m.TaskId == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Task
+                .FirstOrDefaultAsync(m => m.TaskId == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await _context.Task.FindAsync(id);
+            if (task != null)
+            {
+                _context.Task.Remove(task);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Search()
+        {
+            return View(await _context.Task.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Search(int Min_Budget, int Max_Budget)
+        {
+            if (Min_Budget > Max_Budget)
+            {
+                var Search_Task = await _context.Task.Where(m => m.Budget.CompareTo(Max_Budget) >= 0 && m.Budget.CompareTo(Min_Budget) <= 0).ToListAsync();
+                return View(Search_Task);
+            }
+            else
+            {
+                var Search_Task = await _context.Task.Where(m => m.Budget.CompareTo(Min_Budget) >= 0 && m.Budget.CompareTo(Max_Budget) <= 0).ToListAsync();
+                return View(Search_Task);
+            }
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Task.Any(e => e.TaskId == id);
+        }
+    }
+}
---
+++ MVC_HW/MVC_HW/Data/TaskContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_HW.Models;
+
+namespace MVC_HW.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+        public DbSet<Models.Task> Task { get; set; }
+    }
+}
